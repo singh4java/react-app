8e77b9ca145ead7e1ea8de9c02bf1ef9442606c4
manvendrasingh <manvendra80386@gmail.com>
2020-06-12 00:29:59 +0530

    React-app

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,6 +6,7 @@ const person = ( props ) => {
       <p onClick={props.click}>
         I'm a {props.name}! and I'm {props.age} years old!
       </p>
+      <p>Hi its me</p>
       <p>{props.children}</p>
     </div>
   );
