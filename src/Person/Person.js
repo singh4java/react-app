663485ca3907ddce5286cb2accc783a84d556123
manvendrasingh <manvendra80386@gmail.com>
2020-06-12 00:36:10 +0530

    js change

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = ( props ) => {
       <p onClick={props.click}>
         I'm a {props.name}! and I'm {props.age} years old!
       </p>
-      <p>Hi its me</p>
+      <p>Hi its me- i am google</p>
       <p>{props.children}</p>
     </div>
   );
