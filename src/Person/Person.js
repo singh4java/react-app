c65e53a1c5f69a295853ed1501b7ca4dedd3fa6e
manvendrasingh <manvendra80386@gmail.com>
2020-06-12 01:09:24 +0530

    js change

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-const person = ( props ) => {
+const person = (props) => {
   return (
     <div>
       <p onClick={props.click}>
         I'm a {props.name}! and I'm {props.age} years old!
       </p>
-      <p>Hi its me- i am google</p>
       <p>{props.children}</p>
     </div>
   );
